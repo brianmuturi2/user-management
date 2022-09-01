e1ab4aef1ea95e5ca6e6dd7308e108127a20c534
brianmuturi2 <brianmuturi2@gmail.com>
2022-09-01 17:39:22 +0300

    feat(user management): updated e2e test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -8,15 +8,19 @@ describe('My First Test', () => {
     cy.title().should('equal', 'UserManagement');
   });
 
-  it('should have navigation toggle', () => {
-    cy.get('[data-testid="menu-button"]').click();
-  })
-
   it('should have dark theme', () => {
     cy.get('[data-testid="mode-button"]').click();
   })
 
   it('should download users', () => {
     cy.get('[data-testid="csv-button"]').click();
+  })
+
+  it('should have navigation toggle', () => {
+    cy.get('[data-testid="menu-button"]').click();
+  })
+
+  it('should be mobile responsive', () => {
+    cy.viewport('iphone-6');
   })
 })
