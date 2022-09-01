cde9e8f6be232e209aa56de562a8f40a1272e56b
brianmuturi2 <brianmuturi2@gmail.com>
2022-09-01 17:08:01 +0300

    feat(user management): added cypress tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,22 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('app is running!')
+    cy.contains('Users')
+  })
+
+  it('has the correct title', () => {
+    cy.title().should('equal', 'UserManagement');
+  });
+
+  it('should have navigation toggle', () => {
+    cy.get('[data-testid="menu-button"]').click();
+  })
+
+  it('should have dark theme', () => {
+    cy.get('[data-testid="mode-button"]').click();
+  })
+
+  it('should download users', () => {
+    cy.get('[data-testid="csv-button"]').click();
   })
 })
